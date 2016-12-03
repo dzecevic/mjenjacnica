d2e7d4dc1969bdede543cbfab6699a0fff37fcc7
Domagoj Zecevic <domagoj.zecevic@gmail.com>
2016-12-03 18:33:30 +0100

    version 0.01

---
+++ app/src/main/java/info/domagojzecevic/mjenacnica/Mjenkacnica_main.java
@@ -0,0 +1,13 @@
+package info.domagojzecevic.mjenacnica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mjenkacnica_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mjenkacnica_main);
+    }
+}
